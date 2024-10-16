75a4b8947057153329851274990873705581bac4
Jose Javier Columbie <josejaviercolumbie@yahoo.com>
2024-10-16 10:55:12 -0700

    Add project files.

---
+++ XAFWebDataSourceProperty.Module/BusinessObjects/Customer.cs
@@ -0,0 +1,104 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XAFWebDataSourceProperty.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Customer : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Customer(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+
+        decimal salary;
+        int code;
+        string description;
+        string lastName;
+        string name;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => name;
+            set => SetPropertyValue(nameof(Name), ref name, value);
+        }
+
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string LastName
+        {
+            get => lastName;
+            set => SetPropertyValue(nameof(LastName), ref lastName, value);
+        }
+
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Description
+        {
+            get => description;
+            set => SetPropertyValue(nameof(Description), ref description, value);
+        }
+
+
+        public int Code
+        {
+            get => code;
+            set => SetPropertyValue(nameof(Code), ref code, value);
+        }
+
+
+        
+        public decimal Salary
+        {
+
+            get => salary;
+            set => SetPropertyValue(nameof(Salary), ref salary, value);
+        }
+
+
+        [Association("Customer-Transactions")]
+        public XPCollection<Transaction> Transactions
+        {
+            get
+            {
+                return GetCollection<Transaction>(nameof(Transactions));
+            }
+        }
+
+        [Association("Customer-Invoices")]
+        public XPCollection<Invoice> Invoices
+        {
+            get
+            {
+                return GetCollection<Invoice>(nameof(Invoices));
+            }
+        }
+
+
+    }
+}
---
+++ XAFWebDataSourceProperty.Module/BusinessObjects/Transaction.cs
@@ -0,0 +1,96 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XAFWebDataSourceProperty.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Transaction : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Transaction(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+
+
+        CriteriaOperator criteriaProperty;
+        Invoice invoice;
+        Customer customer;
+        double amount;
+        string transactionName;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string TransactionName
+        {
+            get => transactionName;
+            set => SetPropertyValue(nameof(TransactionName), ref transactionName, value);
+        }
+
+
+        public double Amount
+        {
+            get => amount;
+            set => SetPropertyValue(nameof(Amount), ref amount, value);
+        }
+
+
+
+        [Association("Customer-Transactions")]
+        [ImmediatePostData]
+        public Customer Customer
+        {
+            get => customer;
+            set => SetPropertyValue(nameof(Customer), ref customer, value);
+        }
+
+
+        [Association("Invoice-Transactions")]
+        [DataSourceCriteriaProperty(nameof(CriteriaProperty))]
+        public Invoice Invoice
+        {
+            get => invoice;
+            set => SetPropertyValue(nameof(Invoice), ref invoice, value);
+        }
+
+        
+        public CriteriaOperator CriteriaProperty
+        {
+            get 
+            {
+                if (Customer == null) { return CriteriaOperator.Parse("true"); }
+
+                else
+                {
+                  
+
+                   return CriteriaOperator.FromLambda<Invoice>(i => i.Customer.Oid == Customer.Oid && i.Open);
+                }
+            
+            }
+           
+        }
+    }
+}
